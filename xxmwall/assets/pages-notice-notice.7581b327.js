96cf62cdd966bd06e34881ce9c795c155d07ba77
Deng Chang lan <132443093+IIIETR@users.noreply.github.com>
2024-06-16 21:42:11 +0800

    Delete xxmwall directory

--- xxmwall/assets/pages-notice-notice.7581b327.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as r,i as s}from"./index-04a9ae94.js";import{_ as o}from"./plugin-vueexport-helper.1b428a4d.js";const t=o({},[["render",function(o,t){const n=s;return e(),r(n,{class:""})}]]);export{t as default};
